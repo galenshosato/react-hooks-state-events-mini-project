8981717738de28bb6d82133711f633bf324f37e0
Galen Sho Sato <galen.sato@gmail.com>
2023-02-02 03:06:35 -0500

    Added Task List and Category Buttons

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories}) {
+
+  
+  function handleClick(event) {
+    
+  }
+  
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map((category, index) => {
+        return (
+          <button key={index} onClick={handleClick}>{category}</button>
+        )
+      })}
     </div>
   );
 }
